ad714765f8bd394d1edb2db7bdbd24030ea2d6e5
Nick <nconkl1@outlook.com>
2024-06-18 00:06:42 -0400

    :sparkles: added banner output

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,21 @@ use clap::Parser;
 
 mod cli;
 mod parser;
-
+mod banner;
 
 fn main() {
     // Parse the args using the Args struct from cli.rs
     let args = cli::Args::parse();
 
+    // If the user wants to disable colors, we set the override to false
     if args.no_color {
         colored::control::set_override(false);
+    }
+
+    // If the user doesn't want to display the banner, we don't print it
+    if !args.quiet {
+        banner::print_banner();
+        banner::print_info(&args);
     }
 
     let url = parser::url_parser::parse_url(&args.url);
