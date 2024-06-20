59c080cee46160f8cd31363515e5d4e09441641f
Nick <nconkl1@outlook.com>
2024-06-19 23:31:13 -0400

    :art: updated print_info

--- src/banner.rs
+++ src/banner.rs
@@ -23,11 +23,10 @@ pub fn print_banner() {
 
 // Print a pretty block of text with the URL and wordlist
 // We can just use the args struct to get the URL and wordlist
-// If we get to printing the banner and everything, we can assume that the URL and wordlist are valid
 pub fn print_info(args: &cli::Args) {
     // Wrap the URL and wordlist to 80 characters
     const WRAP_WIDTH: usize = 80;
-    let options = Options::new(80);
+    let options = Options::new(120);
 
     let wrapped_url = textwrap::fill(&args.url, &options);
     let wrapped_wordlist = textwrap::fill(&args.wordlist, &options);
@@ -35,17 +34,15 @@ pub fn print_info(args: &cli::Args) {
     // Print the info obtained from the args
     // TODO: Add more information to the banner
     println!("{}", "=".repeat(WRAP_WIDTH));
-    println!("{:<width$} {}", "[+] URL:", wrapped_url, width = 14);
-    println!("{:<width$} {}", "[+] Wordlist:", wrapped_wordlist, width = 14);
+    println!("{:<width$} {}", "[+] URL:", wrapped_url, width = 30);
+    println!("{:<width$} {}", "[+] Wordlist:", wrapped_wordlist, width = 30);
+    println!("{:<width$} {}", "[+] Threads:", args.threads, width = 30);
+    println!("{:<width$} {}", "[+] Negative Status Codes:", args.status_codes_blacklist.join(", "), width = 30);
+    println!("{:<width$} {}", "[+] User Agent:", args.user_agent, width = 30);
     println!("{}", "=".repeat(WRAP_WIDTH));
 }
 
-// ? If I ever change my mind, I can always revert back to the old print_info function
-// pub fn print_info(args: &cli::Args) {
-//     const WIDTH: usize = 14;
-
-//     println!("{}", "=".repeat(80));
-//     println!("{:<width$} {}", "[+] URL:", args.url, width = WIDTH);
-//     println!("{:<width$} {}", "[+] Wordlist:", args.wordlist, width = WIDTH);
-//     println!("{}", "=".repeat(80));
-// }
+pub fn starting_scan() {
+    println!("{}", "Starting scan...".blue());
+    println!("{}", "=".repeat(80));
+}
